cae7a6a7127feb0e596f5518c3aa11bbba7caf04
deelay8 <147777423+deelay8@users.noreply.github.com>
2024-12-16 13:03:53 +0900

    Update config.hpp

--- mvdcc/config.hpp
+++ mvdcc/config.hpp
@@ -2,8 +2,8 @@
 #define CONFIG_HPP
 
 #define PAGE_SIZE 4096
-#define DEFAULT_THREAD_NUM 8       // Default number of threads for debugging
-#define DEFAULT_TUPLE_NUM 1000     // Default number of records for debugging
+#define DEFAULT_THREAD_NUM 64      // Default number of threads for debugging
+#define DEFAULT_TUPLE_NUM 1000000  // Default number of records for debugging
 #define MAX_OPE 10                 // Maximum operations per transaction
 #define EX_TIME 3                  // Execution time in seconds
 #define BATCH_SIZE 50              // Reduced batch size for debugging
